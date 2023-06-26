99c9ba4c4c3c439073845a8994d9719bdd909396
Maria Alejandra Amaya Rodriguez <maamayar@unal.edu.co>
2023-06-25 22:19:12 -0500

    Se crearon los 3 componentes, header,task y tasklist

---
+++ my/src/components/Task.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaTrash } from "react-icons/fa";
+
+const Task =() => {
+    return (
+        <div>
+            <span>
+                <label htmlFor="" className="container-done"></label>
+            </span>
+            <TaskUpdate/>
+            <button className="btn-delete">
+                <FaTrash/>
+            </button>
+        </div>
+    );
+};
+export default Task;
